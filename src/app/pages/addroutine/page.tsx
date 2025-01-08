23477bb2555c6c38c12b14dac34a996c7ec65fe1
MithunPara <paramatm@mcmaster.ca>
2025-01-08 11:50:26 -0500

    adding functionality for admins to create exercise routines

--- src/app/pages/addroutine/page.tsx
+++ src/app/pages/addroutine/page.tsx
@@ -54,11 +54,42 @@ const page = () => {
         })
     }
 
-    const addExerciseToRoutine = () => {}
-    const deleteExerciseFromRoutine = () => {}
+    const addExerciseToRoutine = () => {
+        console.log(exercise)
+
+        if (exercise.name == '' || exercise.details == '' || exercise.sets == 0 || exercise.reps == 0 || exercise.imageFile == null) {
+            toast.error('Please fill all exercise fields', {
+                position: 'top-center'
+            });
+            return;
+        }
+
+        // Add exercise to the workout
+        setRoutine({
+            ...routine,
+            exercises: [...routine.exercises, exercise]
+        })
+        // Clear the exercise state variable to add new exercises
+        // setExercise({
+        //     name: '',
+        //     details: '',
+        //     sets: 0,
+        //     reps: 0,
+        //     imageURL: '',
+        //     imageFile: null
+        // })
+    }
+    const deleteExerciseFromRoutine = (index: number) => {
+        setRoutine({
+            ...routine,
+            exercises: routine.exercises.filter((exercise, indexVal) => indexVal !== index)
+        })
+    }
     const checkAdminLogin = () => {}
     const uploadImage = () => {}
-    const saveRoutine = () => {}
+    const saveRoutine = () => {
+        console.log(routine)
+    }
 
   return (
     <div className='input-form'>
@@ -104,8 +135,88 @@ const page = () => {
             }}    
         />
 
-        
+        <div>
+            <h2 className='title'>Add an Exercise</h2>
 
+            <input 
+                type='text' 
+                placeholder='Exercise Name' 
+                name='name' 
+                value={exercise.name}
+                onChange={handleExerciseChange} 
+            />
+
+            <textarea 
+                placeholder='Exercise Details'
+                value={exercise.details}
+                onChange={(e) => {
+                    setExercise({
+                        ...exercise,
+                        details: e.target.value
+                    })
+                }}
+                rows={5}
+                cols={50}
+            />
+
+            <span>Sets</span>
+            <input 
+                type='number'
+                placeholder='Number of sets'
+                name='sets'
+                value={exercise.sets}
+                onChange={handleExerciseChange}
+            />
+
+            <span>Reps</span>
+            <input 
+                type='number'
+                placeholder='Number of reps'
+                name='reps'
+                value={exercise.reps}
+                onChange={handleExerciseChange}
+            />
+
+            <input 
+                type='file'
+                placeholder='Exercise Image'
+                onChange={(e) => {
+                    setExercise({
+                        ...exercise,
+                        imageFile: e.target.files![0]
+                    })
+                }}    
+            />
+            <button onClick={(e) => {
+                addExerciseToRoutine(e)
+            }}>Add Exercise</button>
+        </div>
+
+        <div className='list-exercises'>
+            <h1 className='title'>Exercises</h1>
+            {
+                routine.exercises.map((exercise, index) => (
+                    <div className='exercise' key={index}>
+                        <h2>{exercise.name}</h2>
+                        <p>{exercise.details}</p>
+                        <span>{exercise.sets}</span>
+                        <span>{exercise.reps}</span>
+
+                        <img src={
+                            exercise.imageFile ? 
+                            URL.createObjectURL(exercise.imageFile) :
+                            exercise.imageURL 
+                        } alt='Exercise Image' />
+
+                        <button onClick={() => deleteExerciseFromRoutine(index)}>Delete Exercise</button>
+                    </div>
+                ))
+            }
+        </div>
+
+        <button onClick={(e) => {
+            saveRoutine(e)
+        }}>Save Routine</button>
     </div>
   )
 }
